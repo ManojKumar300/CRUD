933a3fa515e073470d4f540a785a4aed66db43c1
ManojKumar300 <manojkumarpsr2020@gmail.com>
2024-12-10 18:07:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
-function App() {
+function Gallery2() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [website, setWebsite] = useState('');
     const [result, setResult] = useState([]);
     const [editUser, setEditUser] = useState(null);
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
-            .then((data) => {
-                setResult(data);
-                setLoading(false);
-            })
-            .catch(() => setLoading(false));
+            .then((data) => setResult(data));
     }, []);
 
     function handleData() {
@@ -23,58 +18,57 @@ function App() {
         const userEmail = email.trim();
         const userWebsite = website.trim();
 
-        if (!userName || !userEmail || !userWebsite) {
-            alert("All fields are required!");
-            return;
-        }
-
-        if (editUser) {
-            fetch(`https://jsonplaceholder.typicode.com/users/${editUser.id}`, {
-                method: "PUT",
-                headers: { "Content-Type": "application/json; charset=UTF-8" },
-                body: JSON.stringify({
-                    id: editUser.id,
-                    name: userName,
-                    email: userEmail,
-                    website: userWebsite,
-                }),
-            })
-                .then((res) => res.json())
-                .then((updatedUser) => {
-                    setResult(result.map((user) =>
-                        user.id === updatedUser.id ? updatedUser : user
-                    ));
-                    setEditUser(null);
-                    setName('');
-                    setEmail('');
-                    setWebsite('');
-                });
-        } else {
-            fetch('https://jsonplaceholder.typicode.com/users', {
-                method: "POST",
-                headers: { "Content-Type": "application/json; charset=UTF-8" },
-                body: JSON.stringify({
-                    name: userName,
-                    email: userEmail,
-                    website: userWebsite,
-                }),
-            })
-                .then((res) => res.json())
-                .then((newUser) => {
-                    const userWithId = { ...newUser, id: result.length + 1 };
-                    setResult([...result, userWithId]);
-                    setName('');
-                    setEmail('');
-                    setWebsite('');
-                });
+        if (userName && userEmail && userWebsite) {
+            if (editUser) {
+                // Update existing user
+                fetch(`https://jsonplaceholder.typicode.com/users/${editUser.id}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json; charset=UTF-8",
+                    },
+                    body: JSON.stringify({
+                        id: editUser.id,
+                        name: userName,
+                        email: userEmail,
+                        website: userWebsite,
+                    }),
+                })
+                    .then((res) => res.json())
+                    .then((updatedUser) => {
+                        setResult(result.map((user) =>
+                            user.id === updatedUser.id ? updatedUser : user
+                        ));
+                        setEditUser(null);
+                        setName('');
+                        setEmail('');
+                        setWebsite('');
+                    });
+            } else {
+                // Add new user
+                fetch('https://jsonplaceholder.typicode.com/users', {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json; charset=UTF-8",
+                    },
+                    body: JSON.stringify({
+                        name: userName,
+                        email: userEmail,
+                        website: userWebsite,
+                    }),
+                })
+                    .then((res) => res.json())
+                    .then((newUser) => setResult([...result, newUser]));
+            }
         }
     }
 
-    function handleDelete(id) {
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`, { method: 'DELETE' })
-            .then(() => setResult(result.filter((predata) => predata.id !== id)))
-            .catch((err) => console.log("Error occurred during delete process", err));
-    }
+    function handleDelete(id) {  
+      fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: 'DELETE',
+      })
+          .then(() => setResult(result.filter((predata) => predata.id !== id)))
+          .catch((err) => console.log("Error occurred during delete process", err));
+  }
 
     function handleEdit(user) {
         setName(user.name);
@@ -83,28 +77,24 @@ function App() {
         setEditUser(user);
     }
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
     return (
         <div>
             <label>Enter User Name</label>
-            <input type='text' placeholder='Enter User Name' value={name} onChange={(e) => setName(e.target.value)} /> <br />
+            <input type='text' placeholder='Enter User Name' value={name} onChange={(e) => setName(e.target.value)}></input> <br />
             <label>Enter User Email</label>
-            <input type='email' placeholder='Enter User Email' value={email} onChange={(e) => setEmail(e.target.value)} /> <br />
+            <input type='email' placeholder='Enter User Email' value={email} onChange={(e) => setEmail(e.target.value)}></input> <br />
             <label>Enter User Website</label>
-            <input type='text' placeholder='Enter User Website' value={website} onChange={(e) => setWebsite(e.target.value)} /><br />
+            <input type='text' placeholder='Enter User Website' value={website} onChange={(e) => setWebsite(e.target.value)}></input><br />
             <button onClick={handleData}>{editUser ? "Update User" : "Add New Data"}</button>
 
             <table border={1}>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>User Name</th>
-                        <th>User Email</th>
-                        <th>User Website</th>
-                        <th>Update User Details</th>
+                        <td>ID</td>
+                        <td>User Name</td>
+                        <td>User Email</td>
+                        <td>User Website</td>
+                        <td>Update User Details</td>
                     </tr>
                 </thead>
                 <tbody>
@@ -126,4 +116,4 @@ function App() {
     );
 }
 
-export default App;
+export default Gallery2
